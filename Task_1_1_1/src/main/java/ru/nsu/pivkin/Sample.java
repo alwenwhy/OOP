0e17db6320b84445d5f56c1ddc8936baec044b41
Yuriy Pivkin <y.pivkin@g.nsu.ru>
2025-09-26 22:06:43 +0700

    style changes

--- Task_1_1_1/src/main/java/ru/nsu/pivkin/Sample.java
+++ Task_1_1_1/src/main/java/ru/nsu/pivkin/Sample.java
@@ -1,19 +1,20 @@
 package ru.nsu.pivkin;
 
 public class Sample {
-    private static void heapify(int[] input, int n, int i){
+    private static void heapify(int[] input, int n, int i) {
         int largest = i;
 
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
-        if(left < n && input[left] > input[largest])
+        if (left < n && input[left] > input[largest]) {
             largest = left;
-
-        if(right < n && input[right] > input[largest])
+        }
+        if (right < n && input[right] > input[largest]) {
             largest = right;
+        }
 
-        if(largest != i){
+        if (largest != i) {
             //swap
             int buffer = input[i];
             input[i] = input[largest];
@@ -23,13 +24,14 @@ public class Sample {
         }
     }
 
-    public static void heapSort(int[] input){
+    public static void heapSort(int[] input) {
         int n = input.length;
 
-        for(int i = n / 2 - 1; i >= 0; i--)
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(input, n, i);
+        }
 
-        for(int i = n - 1; i >= 0; i--){
+        for (int i = n - 1; i >= 0; i--) {
             int buffer = input[0];
             input[0] = input[i];
             input[i] = buffer;
@@ -39,21 +41,23 @@ public class Sample {
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int arr[] = {12, 11, 13, 5, 6, 7};
         int n = arr.length;
 
         System.out.print("Source array: ");
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
+        }
         System.out.println();
 
         Sample ob = new Sample();
         ob.heapSort(arr);
 
         System.out.print("Sorted array: ");
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
+        }
         System.out.println();
     }
 }
