f26800319aa0f4541f5c1d7195b34e036f39b476
alwenwhy <alwenwhy@users.noreply.github.com>
2025-10-12 15:14:25 +0000

    Deploying to gh-pages from @ alwenwhy/OOP@2c5a8ed549e7c9670e108bbd0892be923b3944b3 🚀

---
+++ Task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.pivkin","l":"Deck"},{"p":"ru.nsu.pivkin","l":"Game"},{"p":"ru.nsu.pivkin","l":"Player"}];updateSearchResults();
