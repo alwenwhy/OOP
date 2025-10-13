14407b7b630cad9c5213251fe3c674692362a9aa
Черемушкин Евгений Сергеевич <e.cheremushkin@main.nsu.ru>
2025-10-13 09:33:26 +0700

    revert task_1_1_1

---
+++ Task_1_1_1/src/test/java/ru/nsu/pivkin/HeapSortTest.java
@@ -0,0 +1,74 @@
+package ru.nsu.pivkin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapSortTest {
+
+    @Test
+    void testSortEmptyArray() {
+        int[] input = {};
+        int[] expected = {};
+
+        HeapSort.heapSort(input);
+
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    void testSortSingleElement() {
+        int[] input = {5};
+        int[] expected = {5};
+
+        HeapSort.heapSort(input);
+
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    void testSortSimpleArray() {
+        int[] input = {3, 1, 2};
+        int[] expected = {1, 2, 3};
+
+        HeapSort.heapSort(input);
+
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    void testSortAlreadySorted() {
+        int[] input = {1, 2, 3, 4, 5};
+        int[] expected = {1, 2, 3, 4, 5};
+
+        HeapSort.heapSort(input);
+
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    void testSortNegativeNumbers() {
+        int[] input = {-3, 5, -1, 0, -7, 2, 8, -4};
+        int[] expected = {-7, -4, -3, -1, 0, 2, 5, 8};
+
+        HeapSort.heapSort(input);
+
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    void testSortBigValues() {
+        int[] input = {Integer.MAX_VALUE, Integer.MIN_VALUE, 0, -1, 1};
+        int[] expected = {Integer.MIN_VALUE, -1, 0, 1, Integer.MAX_VALUE};
+
+        HeapSort.heapSort(input);
+
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    void checkMain() {
+        Program.main(new String[] {});
+        assertTrue(true);
+    }
+}
